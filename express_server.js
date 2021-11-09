fb60cf21b8c0eb6cb5768cc9dfc2200fd345e074
Alex Thon <ghaiquol@gmail.com>
2021-11-09 21:07:31 +0000

    Add post methods and body-parser

--- express_server.js
+++ express_server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
+
 
 
 const urlDatabase = {
@@ -15,14 +19,29 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+
 app.get('/urls', (req, res)=> {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
 
+
+// Display a empty form to the client
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+// CREATE AND ADD URL TO DB
+app.post("/urls", (req, res) => {
+  const newKey = Math.random().toString(36).substring(2,8);
+  urlDatabase[newKey] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect("/urls");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const id = req.params.shortURL;
-  console.log(typeof id);
+  
   const longURL = urlDatabase[req.params.shortURL];
   console.log(longURL);
   const templateVars = { shortURL: id, longURL: longURL};
