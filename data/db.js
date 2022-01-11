290cb2e51b7f7cee5978d061f379e865b4826d7c
Alex Thon <ghaiquol@gmail.com>
2022-01-11 15:17:38 -0500

    Combine databaseUrl with its helper functions

--- data/db.js
+++ data/db.js
@@ -1,23 +1,55 @@
-const urlsForUserID = (id) => {
-  const userUrls = {};
+// const urlsForUserID = (id) => {
+//   const userUrls = {};
  
-  for (let shortURL in urlDatabase) {
-    let userId = urlDatabase[shortURL].userID;
-    if (id === userId) {
-      userUrls[shortURL] = urlDatabase[shortURL];
-    }
-  }
-  return userUrls;
-};
+//   for (let shortURL in urlDatabase) {
+//     let userId = urlDatabase[shortURL].userID;
+//     if (id === userId) {
+//       userUrls[shortURL] = urlDatabase[shortURL];
+//     }
+//   }
+//   return userUrls;
+// };
 
 
-// SHORT URLS OBJECT DATABASE
+// // SHORT URLS OBJECT DATABASE
+// const urlDatabase = {
+//   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+//   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW"}
+// };
+
+// module.exports = {
+//   urlsForUserID,
+//   urlDatabase
+// }
+
 const urlDatabase = {
-  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
-  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW"}
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+  },
+
+  // helper functions
+  getLongUrl: function(shortURL) {
+    return this[shortURL].longURL;
+  },
+  setShortUrl: function (shortURL, longURL, userID) {
+    this[shortURL] = { longURL: longURL, userID: userID };
+  },
+  setLongUrl: function (shortURL, longURL) {
+    this[shortURL].longURL = longURL;
+  },
+  urlsForUserID: function (id) {
+    let tmp = {};
+    for (const url in this) {
+      if (this[url].userID === id)
+        tmp[url] = this[url];
+    }
+    return tmp;
+  }
 };
 
-module.exports = {
-  urlsForUserID,
-  urlDatabase
-}
+module.exports = urlDatabase
